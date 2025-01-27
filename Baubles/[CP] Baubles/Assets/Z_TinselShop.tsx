d044a8f5ba47081a6293ce7f782b89f0d8da86ee
Omar Crespo <crespomerchano@gmail.com>
2025-01-26 19:25:31 -0500

    v1.2.0

---
+++ Baubles/[CP] Baubles/Assets/Z_TinselShop.tsx
@@ -0,0 +1,92 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Z_TinselShop" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="Z_TinselShop.png" width="128" height="128"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="500"/>
+   <frame tileid="2" duration="500"/>
+   <frame tileid="4" duration="500"/>
+   <frame tileid="6" duration="500"/>
+   <frame tileid="10" duration="500"/>
+   <frame tileid="12" duration="500"/>
+   <frame tileid="14" duration="500"/>
+   <frame tileid="18" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="500"/>
+   <frame tileid="3" duration="500"/>
+   <frame tileid="5" duration="500"/>
+   <frame tileid="7" duration="500"/>
+   <frame tileid="11" duration="500"/>
+   <frame tileid="13" duration="500"/>
+   <frame tileid="15" duration="500"/>
+   <frame tileid="19" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="8" duration="500"/>
+   <frame tileid="8" duration="500"/>
+   <frame tileid="8" duration="500"/>
+   <frame tileid="20" duration="500"/>
+   <frame tileid="21" duration="500"/>
+   <frame tileid="21" duration="500"/>
+   <frame tileid="21" duration="500"/>
+   <frame tileid="22" duration="500"/>
+   <frame tileid="8" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <animation>
+   <frame tileid="9" duration="300"/>
+   <frame tileid="29" duration="300"/>
+   <frame tileid="28" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="32">
+  <animation>
+   <frame tileid="32" duration="500"/>
+   <frame tileid="34" duration="500"/>
+   <frame tileid="36" duration="500"/>
+   <frame tileid="38" duration="500"/>
+   <frame tileid="42" duration="500"/>
+   <frame tileid="44" duration="500"/>
+   <frame tileid="46" duration="500"/>
+   <frame tileid="50" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="33">
+  <animation>
+   <frame tileid="33" duration="500"/>
+   <frame tileid="35" duration="500"/>
+   <frame tileid="37" duration="500"/>
+   <frame tileid="39" duration="500"/>
+   <frame tileid="43" duration="500"/>
+   <frame tileid="45" duration="500"/>
+   <frame tileid="47" duration="500"/>
+   <frame tileid="51" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="40">
+  <animation>
+   <frame tileid="40" duration="500"/>
+   <frame tileid="40" duration="500"/>
+   <frame tileid="40" duration="500"/>
+   <frame tileid="52" duration="500"/>
+   <frame tileid="53" duration="500"/>
+   <frame tileid="53" duration="500"/>
+   <frame tileid="53" duration="500"/>
+   <frame tileid="52" duration="500"/>
+   <frame tileid="40" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="41">
+  <animation>
+   <frame tileid="41" duration="300"/>
+   <frame tileid="61" duration="300"/>
+   <frame tileid="60" duration="300"/>
+  </animation>
+ </tile>
+</tileset>
